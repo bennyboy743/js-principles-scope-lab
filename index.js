f23539e80a630adc7cd4bf0c39011e2739058f8b
Benjamin <benjaminrowe0@gmail.com>
2021-12-24 07:58:16 +1100

    have fixed the problem

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(name){
+    bestCustomer = name
+}
+const leastFavoriteCustomer = "Ben"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Josh"
+}
